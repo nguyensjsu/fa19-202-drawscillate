67f9f051c86ac05e593987bda619dcc43ffd8320
Aditya Agrawal <55107922+agrawaladit@users.noreply.github.com>
2019-11-11 23:42:36 -0800

    Update Drawscillate.java
    Task #3

--- src/main/java/drawscillate/Drawscillate.java
+++ src/main/java/drawscillate/Drawscillate.java
@@ -2,6 +2,8 @@ package drawscillate;
 
 import processing.core.PApplet;
 import processing.sound.*;
+import controlP5.*;
+import java.util.*;
 
 public class Drawscillate extends PApplet {
     private SinOsc[] sineWaves; // Array of sines
@@ -28,6 +30,20 @@ public class Drawscillate extends PApplet {
             // Set the amplitudes for all oscillators
             sineWaves[i].amp((float) sineVolume);
         }
+        
+        //Drop Down
+        size(400, 400);
+        cp5 = new ControlP5(this);
+        List l = Arrays.asList("Star", "Rectangle", "Heart", "Circle");
+        /* add a ScrollableList, by default it behaves like a DropdownList */
+        cp5.addScrollableList("dropdown")
+         .setPosition(100, 100)
+         .setSize(200, 100)
+         .setBarHeight(20)
+         .setItemHeight(20)
+         .addItems(l)
+         // .setType(ScrollableList.LIST) // currently supported DROPDOWN and LIST
+         ;
     }
 
     public void draw() {
@@ -43,10 +59,22 @@ public class Drawscillate extends PApplet {
             // Set the frequencies for all oscillators
             sineWaves[i].freq(sineFreq[i]);
         }
+        
+        //Drop Down
+        background(240);
     }
 
     public void mousePressed(){
         background(64);
+    }
+    
+    public void dropdown(int n) {
+        /* request the selected item based on index n */
+        println(n, cp5.get(ScrollableList.class, "dropdown").getItem(n));
+
+        CColor c = new CColor();
+        c.setBackground(color(255,0,0));
+        cp5.get(ScrollableList.class, "dropdown").getItem(n).put("color", c);
     }
 
     public static void main(String[] args){
