7205ffff5b2490ec7adfb307082c2f7aaa30a440
ihetal <hetal.shah@sjsu.edu>
2019-11-18 23:58:20 -0800

    Implementing Observer Pattern

--- src/main/java/drawscillate/AppController.java
+++ src/main/java/drawscillate/AppController.java
@@ -2,10 +2,10 @@ package drawscillate;
 
 import processing.core.PApplet;
 
-public class AppController extends PApplet {
-    
-    private int gameScreen =0;
-    private WelcomeScreen welcomeScreen;
+public class AppController extends PApplet implements IScreenObserver{
+
+    private IScreen welcomeScreen;
+    private IScreen current;
     public void settings() {
         size(1000, 750);
         smooth();
@@ -14,13 +14,29 @@ public class AppController extends PApplet {
     public void setup() {
         background(74, 73, 70);
         welcomeScreen = new WelcomeScreen(this);
+        current = welcomeScreen;
     }
     
     public void draw() {
-        if(gameScreen ==0) {
-            welcomeScreen.display();
-        }
+        current.display();
     }
+    
+    public void mousePressed() {
+        current.touch();
+        
+    }
+    
+    public void screenChange(int gameScreen) {
+        switch (gameScreen) {
+        case 0:
+            current = welcomeScreen;
+            break;
+        default:
+            current = welcomeScreen;
+        }
+        
+    }
+    
     public static void main(String[] args) {
         String[] processingArgs = { "MySketch" };
         AppController mySketch = new AppController();
--- src/main/java/drawscillate/WelcomeScreen.java
+++ src/main/java/drawscillate/WelcomeScreen.java
@@ -3,15 +3,19 @@ package drawscillate;
 import processing.core.PApplet;
 import processing.core.PConstants;
 
-public class WelcomeScreen implements IScreen{
-    
+public class WelcomeScreen implements IScreen, IScreenSubject {
+
     PApplet parent;
-    
-    WelcomeScreen(PApplet p){
+    IScreenObserver observer;
+    private int gameScreen;
+
+    WelcomeScreen(PApplet p) {
         parent = p;
-        
+        gameScreen = 0;
+        attach((IScreenObserver)p);
+
     }
-    
+
     public void display() {
         parent.textAlign(PConstants.CENTER);
         parent.fill(237, 97, 21);
@@ -21,15 +25,33 @@ public class WelcomeScreen implements IScreen{
         parent.text("Click to start", parent.width / 2, parent.height - 30);
     }
 
-    /** 
-     * Yet to implement this function
+    /**
+     * Touch Event
      */
     public void touch() {
-        // TODO Auto-generated method stub
-        
+        gameScreen = 1;
+        notifyObservers();
+
     }
 
-    
-    
+    /**
+     * Add Observer to Subscribers List
+     * 
+     * @param obj Observer Object
+     */
+    public void attach(IScreenObserver obj) {
+        
+        observer = obj;
+
+    }
+
+    /**
+     * Trigger Events to Observers
+     */
+    public void notifyObservers() {
+        
+        observer.screenChange(gameScreen);
+
+    }
 
 }
