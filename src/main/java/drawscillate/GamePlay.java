32ab774629dee5a899183c75be0fca2a1c077ff8
Rajeev Sebastian <rajeev.sebastian@sjsu.edu>
2019-11-19 23:46:36 -0800

    back button implementation

--- src/main/java/drawscillate/GamePlay.java
+++ src/main/java/drawscillate/GamePlay.java
@@ -8,6 +8,8 @@ public class GamePlay implements IScreen {
     PApplet parent;
     IScreenObserver observer;
     private int gameScreen;
+    private int goBackbuttonX;
+    private int goBackbuttonY;
 
     GamePlay(PApplet p) {
         parent = p;
@@ -21,6 +23,11 @@ public class GamePlay implements IScreen {
         parent.fill(237, 97, 21);
         parent.textSize(25);
         parent.text("Game Screen", parent.width / 2, parent.height - 30);
+        goBackbuttonX = (parent.width * 5) / 6;
+        goBackbuttonY = (parent.height) / 6;
+        //parent.rect(goBackbuttonX, goBackbuttonY, 100 , 50 );
+        parent.textAlign(PConstants.LEFT);
+        parent.text("Main Menu",goBackbuttonX,goBackbuttonY);
     }
 
     /**
@@ -28,8 +35,22 @@ public class GamePlay implements IScreen {
      */
     public void touch() {
         //Need to implement this function
-
+         if(overRect(150,50)) {
+             gameScreen = 1;
+             notifyObservers();
+         }   
     }
+    
+    boolean overRect(int width, int height)  {
+        System.out.println("X:" + parent.mouseX);
+        System.out.println("Y:" + parent.mouseY);
+        if (parent.mouseX >= goBackbuttonX && parent.mouseX <= goBackbuttonX + width && 
+                parent.mouseY >= goBackbuttonY - 20 && parent.mouseY <= goBackbuttonY + height) {
+          return true;
+        } else {
+          return false;
+        }
+      }
 
     /**
      * Add Observer to Subscribers List
