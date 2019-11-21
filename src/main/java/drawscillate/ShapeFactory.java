b98b81846db9383fb535ff42b5cb72ca6493deca
Shraddha Nayak <shraddhanayak07@gmail.com>
2019-11-21 00:36:01 -0800

    Update ShapeFactory.java

--- src/main/java/drawscillate/ShapeFactory.java
+++ src/main/java/drawscillate/ShapeFactory.java
@@ -7,6 +7,8 @@ public class ShapeFactory {
         switch(type) {
         case "Heart": return new Heart();
         case "Star": return new Star();
+        case "Rectangle": return new Rectangle();
+        case "Circle": return new Circle();
         }
         return null;
         
