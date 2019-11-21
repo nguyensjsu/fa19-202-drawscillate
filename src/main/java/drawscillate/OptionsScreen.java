e3053fb07ed90318e96df5531df3efe18a4ed388
ihetal <hetal.shah@sjsu.edu>
2019-11-21 00:38:57 -0800

    Fixing the issue

--- src/main/java/drawscillate/OptionsScreen.java
+++ src/main/java/drawscillate/OptionsScreen.java
@@ -5,6 +5,7 @@ import controlP5.CallbackListener;
 import controlP5.ControlP5;
 import controlP5.Controller;
 import controlP5.DropdownList;
+import controlP5.Textarea;
 import processing.core.PApplet;
 
 import java.util.Arrays;
@@ -13,6 +14,8 @@ import java.util.HashSet;
 
 import static processing.core.PConstants.CENTER;
 import static processing.core.PConstants.LEFT;
+import static processing.core.PConstants.RIGHT;
+import static processing.core.PConstants.TOP;
 
 public class OptionsScreen implements IScreen, CallbackListener {
     private PApplet applet;
@@ -23,6 +26,7 @@ public class OptionsScreen implements IScreen, CallbackListener {
     private String shapeSelection;
     private Collection<OptionsScreenObserver> optionsScreenObservers = new HashSet<>();
     private AppController app;
+    private Textarea myTextArea;
 
     OptionsScreen(PApplet applet) {
         this.applet = applet;
@@ -34,11 +38,11 @@ public class OptionsScreen implements IScreen, CallbackListener {
             .onChange(this)
             .setOpen(false)
             .setBarVisible(false)
-            .setPosition(270, 200)
+            .setPosition(applet.width-300, applet.height-300)
             .setSize(200, 100)
             .setHeight(300)
-            .setBarHeight(30)
-            .setItemHeight(30)
+            .setBarHeight(40)
+            .setItemHeight(40)
             .addItems(Arrays.asList("Easy", "Normal", "Hard"));
 
         // create a second DropdownList
@@ -47,12 +51,26 @@ public class OptionsScreen implements IScreen, CallbackListener {
             .onChange(this)
             .setOpen(false)
             .setBarVisible(false)
-            .setPosition(30, 200)
+            .setPosition(applet.width-300, applet.height-350)
             .setSize(200, 100)
             .setHeight(300)
-            .setBarHeight(30)
-            .setItemHeight(30)
+            .setBarHeight(40)
+            .setItemHeight(40)
             .addItems(Arrays.asList("Star", "Rectangle", "Heart", "Circle"));
+        
+        myTextArea = controlP5
+                    .addTextarea("txt")
+                    .setPosition(applet.width/10f+10, applet.height/2f)
+                    .setSize(applet.width-350 ,applet.height-300)
+                    .setVisible(false)
+                    .setFont(applet.createFont("Georgia",14))
+                    .setLineHeight(18)
+                    .setColor(applet.color(237, 97, 21))
+                    .setText("Key Controls:\n\n"
+                        +" y - Yellow\n r - Red\n"
+                        +" o - Green\n b - Blue\n"
+                        +" p - Purple \n g - Orange\n"
+                        );
 
         button = new Button(applet, "Play!");
     }
@@ -70,22 +88,40 @@ public class OptionsScreen implements IScreen, CallbackListener {
     public void willDisplay() {
         d1.setBarVisible(true);
         d2.setBarVisible(true);
+        myTextArea.setVisible(true);
     }
 
     @Override
     public void willStopDisplaying() {
         d1.setBarVisible(false);
         d2.setBarVisible(false);
+        myTextArea.setVisible(false);
     }
 
     @Override
     public void display() {
         //Text
-        applet.background(74, 73, 70);
+        applet.background(0);
         applet.textAlign(CENTER);
         applet.fill(237, 97, 21);
         applet.textSize(20);
-        applet.text("Hi "+app.getName()+"! Select your Preferences!", applet.width / 2f, applet.width / 5f);
+        applet.text("Hi "+app.getName()+"! Select your Preferences!", applet.width / 2f, applet.width / 10f);
+        applet.noFill();
+        applet.stroke(0, 153, 204);
+        applet.rect(applet.height/10f, applet.width/9f, applet.height-100, applet.width-100);
+        applet.fill(48, 145, 50);
+        applet.textSize(20);
+        applet.textAlign(LEFT);
+        applet.text("How to play:", applet.height /10f+10, applet.width / 6f);
+        applet.fill(255);
+        applet.textAlign(LEFT,TOP);
+        applet.textSize(14);
+        applet.text("Move your cursor through the outline of the picture,\n"
+                + "without crossing over the border to win!", applet.height/10f+10 , applet.width / 9f+35 );
+        applet.textSize(13);
+        applet.textAlign(RIGHT);
+        applet.text("CHOOSE SHAPE",applet.width-315, applet.height-325);
+        applet.text("CHOOSE DIFFCULTY",applet.width-315, applet.height-275);
 
         // draw the button
         applet.textAlign(LEFT);
