1bde8afebbdadd3afeb91b9fdb34dd6d8a1eea23
ihetal <hetal.shah@sjsu.edu>
2019-11-21 00:13:51 -0800

    Revert "Adding content to game options screen"
    This reverts commit 999d6ac827b1f24a879359ce041dfbd27531e648.

--- src/main/java/drawscillate/OptionsScreen.java
+++ src/main/java/drawscillate/OptionsScreen.java
@@ -5,7 +5,6 @@ import controlP5.CallbackListener;
 import controlP5.ControlP5;
 import controlP5.Controller;
 import controlP5.DropdownList;
-import controlP5.Textarea;
 import processing.core.PApplet;
 import processing.core.PConstants;
 
@@ -25,7 +24,6 @@ public class OptionsScreen implements IScreen, CallbackListener {
     private String shapeSelection;
     private Collection<OptionsScreenObserver> optionsScreenObservers = new HashSet<>();
     private AppController app;
-    private Textarea myTextarea;
 
     OptionsScreen(PApplet applet) {
         this.applet = applet;
@@ -37,12 +35,11 @@ public class OptionsScreen implements IScreen, CallbackListener {
             .onChange(this)
             .setOpen(false)
             .setBarVisible(false)
-            .setPosition(applet.width-300, applet.height-300)
+            .setPosition(270, 200)
             .setSize(200, 100)
             .setHeight(300)
-            .setBarHeight(40)
-            .setItemHeight(40)
-            .setColorBackground(applet.color(0, 153, 204, 99))
+            .setBarHeight(30)
+            .setItemHeight(30)
             .addItems(Arrays.asList("Easy", "Normal", "Hard"));
 
         // create a second DropdownList
@@ -51,25 +48,12 @@ public class OptionsScreen implements IScreen, CallbackListener {
             .onChange(this)
             .setOpen(false)
             .setBarVisible(false)
-            .setPosition(applet.width-300, applet.height-350)
+            .setPosition(30, 200)
             .setSize(200, 100)
             .setHeight(300)
-            .setBarHeight(40)
-            .setItemHeight(40)
+            .setBarHeight(30)
+            .setItemHeight(30)
             .addItems(Arrays.asList("Star", "Rectangle", "Heart", "Circle"));
-        
-        myTextarea = controlP5.addTextarea("txt")
-                .setPosition(applet.width/10f+10, applet.height/2f)
-                .setSize(applet.width -100 ,applet.height)
-                .setVisible(false)
-                .setFont(applet.createFont("arial",16))
-                .setLineHeight(20)
-                .setColor(applet.color(237, 97, 21));
-        myTextarea.setText("Key Controls:\n\n"
-                +" y - Yellow          r - Red\n"
-                +" o - Green          b - Blue\n"
-                +" p - Purple          g - Orange\n"
-                  );
 
         button = new Button(applet, "Play!");
     }
@@ -87,14 +71,12 @@ public class OptionsScreen implements IScreen, CallbackListener {
     public void willDisplay() {
         d1.setBarVisible(true);
         d2.setBarVisible(true);
-        myTextarea.setVisible(true);
     }
 
     @Override
     public void willStopDisplaying() {
         d1.setBarVisible(false);
         d2.setBarVisible(false);
-        myTextarea.setVisible(false);
     }
 
     @Override
@@ -117,10 +99,6 @@ public class OptionsScreen implements IScreen, CallbackListener {
         applet.textSize(14);
         applet.text("Move your cursor through the outline of the picture,\n"
                 + "without crossing over the border to win!", applet.height/10f+10 , applet.width / 9f+35 );
-        applet.textSize(13);
-        applet.textAlign(PConstants.RIGHT);
-        applet.text("CHOOSE SHAPE",applet.width-315, applet.height-325);
-        applet.text("CHOOSE DIFFCULTY",applet.width-315, applet.height-275);
 
         // draw the button
         applet.textAlign(LEFT);
