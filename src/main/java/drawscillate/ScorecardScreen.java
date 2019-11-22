cbaf06a863dddd0758af6aff1aa6b0aceae551d4
Aditya Agrawal <aditya.agrawal@sjsu.edu>
2019-11-21 21:14:57 -0800

    star scoring strategy

--- src/main/java/drawscillate/ScorecardScreen.java
+++ src/main/java/drawscillate/ScorecardScreen.java
@@ -1,6 +1,33 @@
 package drawscillate;
 
+import processing.core.PApplet;
+
 public class ScorecardScreen implements IScreen {
+
+    private PApplet applet;
+    private int hits;
+    private IScoringStrategy scoringStrategy;
+
+    public ScorecardScreen(String shape, int hits) {
+        this.hits = hits;
+
+        switch (shape) {
+            case "Heart":
+                scoringStrategy = new HeartScoringStrategy();
+                break;
+            case "Circle":
+                scoringStrategy = new CircleScoringStrategy();
+                break;
+            case "Rectangle":
+                scoringStrategy = new RectangleScoringStrategy();
+                break;
+            case "Star":
+                scoringStrategy = new StarScoringStrategy();
+                break;
+        }
+    }
+
+
     @Override
     public void mousePressed() {
 
@@ -8,7 +35,11 @@ public class ScorecardScreen implements IScreen {
 
     @Override
     public void display() {
+        applet.text("Drawscillate", applet.width / 2f, applet.height / 2f);
+    }
 
+    private int getScore(IScoringStrategy scoringStrategy){
+        return scoringStrategy.CalculateScore(hits);
     }
 
     @Override
