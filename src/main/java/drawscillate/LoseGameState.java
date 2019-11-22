8841c93338dc03cb54dfb7e23e0dd10d5457f835
Rajeev Sebastian <rajeev.sebastian@sjsu.edu>
2019-11-21 18:20:18 -0800

    lose game state

---
+++ src/main/java/drawscillate/LoseGameState.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package drawscillate;
+
+/**
+ * @author rajee
+ *
+ */
+public class LoseGameState implements IGameState {
+
+    /**
+    * 
+    * Function name - handleMouseEvent
+    * Description   - 
+    * @param     - 
+    * @return        - 
+    */
+    @Override
+    public void handleMouseEvent() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
