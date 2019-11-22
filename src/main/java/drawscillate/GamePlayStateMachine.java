a71b56336a8d1b29e347bb1f558821dceec69d58
Rajeev Sebastian <rajeev.sebastian@sjsu.edu>
2019-11-21 18:09:09 -0800

    merge issues
    state machine
    
    state machine interface
    
    lose game state
    
    win game state
    
    initial game state
    
    in play game state
    
    i game state
    
    merge issue
    
    merge issues
    
    state machine
    
    state machine interface
    
    lose game state
    
    win game state
    
    initial game state
    
    in play game state
    
    i game state
    
    merge issues
    
    merge issues
    
    state machine
    
    state machine interface
    
    lose game state
    
    win game state
    
    initial game state
    
    in play game state
    
    i game state
    
    merge issue

---
+++ src/main/java/drawscillate/GamePlayStateMachine.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package drawscillate;
+
+public class GamePlayStateMachine implements IGamePlayStateMachine {
+
+    
+    
+    
+    
+    /**
+     * 
+     */
+    public GamePlayStateMachine() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    /**
+    * 
+    * Function name - setStateInitial
+    * Description   - 
+    * @param     - 
+    * @return        - 
+    */
+    @Override
+    public void setStateInitial() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+    * 
+    * Function name - setStateInPlay
+    * Description   - 
+    * @param     - 
+    * @return        - 
+    */
+    @Override
+    public void setStateInPlay() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+    * 
+    * Function name - setStateWin
+    * Description   - 
+    * @param     - 
+    * @return        - 
+    */
+    @Override
+    public void setStateWin() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    /**
+    * 
+    * Function name - setStateLose
+    * Description   - 
+    * @param     - 
+    * @return        - 
+    */
+    @Override
+    public void setStateLose() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
