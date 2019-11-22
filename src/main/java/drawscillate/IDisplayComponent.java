45029612c523749085a34b32a1e91c53ae8b1765
ihetal <hetal.shah@sjsu.edu>
2019-11-21 20:11:28 -0800

    Create IDisplayComponent.java

---
+++ src/main/java/drawscillate/IDisplayComponent.java
@@ -0,0 +1,18 @@
+package drawscillate;
+
+
+/** Display Component Interface */
+public interface IDisplayComponent
+{
+    /**
+     * Return Display Component Contents
+     * @return Display Component Contents
+     */
+    void display() ;
+
+    /**
+     * Add A Child Component
+     * @param c Child Component
+     */
+    void addSubComponent( IDisplayComponent c ) ;
+}
