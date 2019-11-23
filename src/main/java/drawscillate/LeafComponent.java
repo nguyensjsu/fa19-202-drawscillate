44fdb299735e817a1e92f2dffd5eb8c34b69c537
Aditya Agrawal <aditya.agrawal@sjsu.edu>
2019-11-23 11:33:11 -0800

    aaaa

--- src/main/java/drawscillate/LeafComponent.java
+++ src/main/java/drawscillate/LeafComponent.java
@@ -2,5 +2,5 @@ package drawscillate;
 
 public abstract class LeafComponent implements IDisplayComponent {
     @Override
-    public final void addSubComponent(IDisplayComponent c) {}
+    public void addSubComponent(IDisplayComponent c) {}
 }
